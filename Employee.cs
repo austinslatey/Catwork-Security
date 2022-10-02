e5942b696602592a6498c72f9775a86ee3767043
Mr WhiteKidd <austinslater58@gmail.com>
2022-10-02 15:43:57 -0500

    Constructed a Program class as well as an Employee class to create users using the terminal Created, declared, called, and instantiated new classes Refractored code along the wat to keep the project organized, maintanable, and extensible

---
+++ Employee.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections.Generic;
+
+namespace CatWorx.BadgeMaker
+{
+    class Employee
+    {
+        // Employee class
+        private string FirstName;
+        private string LastName;
+        private int Id;
+        private string PhotoUrl;
+        public Employee(string firstName, string lastName, int id, string photoUrl) 
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Id = id;
+            PhotoUrl = photoUrl;
+        }
+
+        public string GetFullName() {
+            return FirstName + " " + LastName;
+        }
+
+        public int GetId() 
+        {
+            return Id;
+        }
+
+        public string GetPhotoUrl() 
+        {
+            return PhotoUrl;
+        }   
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace CatWorx.BadgeMaker
+{
+    class Program
+{
+  static List<Employee> GetEmployees()
+  {
+    List<Employee> employees = new List<Employee>();
+    while (true)
+    {
+      // Move the initial prompt inside the loop, so it repeats for each employee
+      Console.WriteLine("Please first name: (leave empty to exit): ");
+
+      // Change input to firstName
+      string firstName = Console.ReadLine() ?? "";
+
+      if (firstName == "")
+      {
+        break;
+      }
+
+      // Add a console.ReadLine() for each value
+      Console.WriteLine("Enter last name: ");
+      string lastName = Console.ReadLine() ?? "";
+      Console.Write("Enter ID: ");
+      int id = Int32.Parse(Console.ReadLine() ?? "");
+      Console.Write("Enter Photo URL: ");
+      string photoUrl = Console.ReadLine() ?? "";
+
+      // Create a new Employee instance
+      Employee currentEmployee = new Employee(firstName, lastName, id, photoUrl);
+      // Add currentEmployee not a string  
+      employees.Add(currentEmployee);
+    }
+    // Update the metod return type
+
+    return employees;
+  }
+
+  static void PrintEmployees(List<Employee> employees)
+  {
+    for (int i = 0; i < employees.Count; i++)
+    {
+      // each item in employees is an Employee instance
+      string template = "{0,-10}\t{1,-20}\t{2}";
+      Console.WriteLine(String.Format(template, employees[i].GetId(), employees[i].GetFullName(), employees[i].GetPhotoUrl()));
+    }
+  }
+
+  static void Main(string[] args)
+  {
+    List<Employee> employees = GetEmployees();
+    PrintEmployees(employees);
+  }
+}
+}
